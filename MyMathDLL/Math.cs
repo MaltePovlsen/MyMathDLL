d9fb0c1a1ca4cf1fb58be91ccd265420adcb3c29
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2018-09-17 12:21:47 +0200

    d

--- MyMathDLL/Math.cs
+++ MyMathDLL/Math.cs
@@ -8,6 +8,12 @@ namespace MyMathDLL
 {
     public class Math
     {
+        /// <summary>
+        /// Add numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int Add(int a, int b)
         {
             return a + b;
@@ -17,7 +23,12 @@ namespace MyMathDLL
         {
             return a + b + c;
         }
-
+        /// <summary>
+        /// Multiply numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public double Multiply(double a, double b)
         {
             return a * b;
@@ -27,7 +38,12 @@ namespace MyMathDLL
         {
             return a * b * c;
         }
-
+        /// <summary>
+        /// subtract
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int Subtract(int a, int b)
         {
             return a - b;
